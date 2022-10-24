6b89fc4c752db8da4c0781f4a2d39429dfd136c2
Gabriel Raimundo <gabriel.alesiunas@yahoo.com.br>
2022-10-24 18:16:14 -0300

    Textos nas telas

---
+++ componentes/estilos.js
@@ -0,0 +1,53 @@
+import { StyleSheet } from "react-native";
+
+const estilos = StyleSheet.create({
+  card: {
+    width: 250,
+    aligItems: "center",
+    backgroundColor: '#107c0f',
+    flexDirection: "row",
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    marginBottom: 20, 
+  },  
+
+  card: {
+    width: 320,
+    height: 180,
+    marginBottom: 10,
+  },
+
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: '#2c2e2d',
+    padding: 15,
+  },
+
+  img: {
+    width: 80,
+    height: 80,
+    borderRadius: 100,
+    marginBottom: 10,
+  },
+
+  titulo: {
+    fontSize: 26,
+    fontWeight: "bold",
+    color: "#107c0f",
+    marginBottom: 5,
+  },
+
+  descricao: {
+
+  },
+  
+
+
+});
+
+
+
+export default estilo;
