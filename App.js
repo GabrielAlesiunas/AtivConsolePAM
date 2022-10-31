ece6b69fe71ad9739fdfb836e2b3926462c0e2d9
Gabriel Raimundo <gabriel.alesiunas@yahoo.com.br>
2022-10-31 16:59:36 -0300

    Projeto finalizado!!

--- App.js
+++ App.js
@@ -1,47 +1,11 @@
-import React from "react";
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import Telas from './componentes/Stack/Stack';
 
-import TelaInicial from "./componentes/TelaInicial";
-import TelaConsoles from "./componentes/TelaConsoles";
-import TelaJogos from "./componentes/TelaJogos";
-import TelaNuvem from "./componentes/TelaNuvem";
-import { Text } from "react-native";
-
-const Menu = createStackNavigator().Navigator;
-const Itens = createStackNavigator().Screen;
-
-export default function App () {
+export default function App() {
   return (
-  //   <NavigationContainer >
-  //     <Stack.Navigator>
-  //       <Stack.Screen name="Tela Inicial" componentes = { TelaInicial } />
-  //       <Stack.Screen name="Consoles" componentes = { TelaConsoles } />
-  //       <Stack.Screen name="Jogos" componentes = { TelaJogos } />
-  //       <Stack.Screen name="Nuvem" componentes = { TelaNuvem } />
-  //     </Stack.Navigator>
-  //   </NavigationContainer>
-  // );
-  <NavigationContainer>
-    <Menu>
-      <Itens
-        name="Início"
-        component={ TelaInicial }
-      />
-      <Itens
-        name="Consoles"
-        component={TelaConsoles}
-      />
-      
-      <Itens
-        name="Nuvem"
-        component={TelaNuvem}
-      />
-      <Itens
-        name="Jogos"
-        component={TelaJogos }
-      />
-    </Menu>
-  </NavigationContainer>
+    <NavigationContainer>
+      <Telas/>
+    </NavigationContainer>
   );
 }
